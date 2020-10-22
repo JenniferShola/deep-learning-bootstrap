7f4718a55ebc157b4a0a7a71b548db5cbbd43759
Alethea Power <10975066+aletheap@users.noreply.github.com>
2020-10-22 14:21:25 -0700

    lspci and nvidia-smi

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import torch
+import subprocess
+
 
 cuda = torch.cuda.is_available()
 devices = torch.cuda.device_count()
 
+print("lspci:")
+subprocess.run("lspci | grep -i nvidia", shell=True)
+print("nvidia-smi:")
+
+subprocess.run("nvidia-smi", shell=True)
 
 print("CUDA Available:", cuda)
 
